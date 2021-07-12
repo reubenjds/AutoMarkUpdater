2c608e6deb9ef5d5190bd25f9a048818190d9339
reubenjds <46090211+reubenjds@users.noreply.github.com>
2021-07-11 20:37:28 -0400

    Update settings.py

--- automark/automark/settings.py
+++ automark/automark/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8dmk8*%n!72boy*chj8z#+eak2lf7!h3fdk(bi_qjm#k6gfo#z'
+# Add your own secret key
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
